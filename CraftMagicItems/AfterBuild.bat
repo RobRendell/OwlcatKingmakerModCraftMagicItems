d23a0cff56d23f65a098e641c0f7532afbaafd4c
Rob Rendell <rob.rendell.au@gmail.com>
2018-12-17 08:56:39 +1100

    Fix error in AfterBuild bat file.

--- CraftMagicItems/AfterBuild.bat
+++ CraftMagicItems/AfterBuild.bat
@@ -6,15 +6,12 @@ xcopy CraftMagicItems.dll CraftMagicItems || goto :error
 xcopy ..\..\..\Info.json CraftMagicItems || goto :error
 xcopy /E /I ..\..\..\L10n CraftMagicItems\L10n || goto :error
 xcopy /E /I ..\..\..\Icons CraftMagicItems\Icons || goto :error
-xcopy /E /I ..\..\..\Icons CraftMagicItems\Data || goto :error
+xcopy /E /I ..\..\..\Data CraftMagicItems\Data || goto :error
 "C:\Program Files\7-Zip\7z.exe" a CraftMagicItems.zip CraftMagicItems || goto :error
 
 "C:\Program Files\7-Zip\7z.exe" a CraftMagicItems-Source.zip ..\..\*.cs ../../../L10n || goto :error
 
-xcopy /Y CraftMagicItems.dll "C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Kingmaker\Mods\CraftMagicItems" || goto :error
-xcopy /E /I /Y ..\..\..\L10n "C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Kingmaker\Mods\CraftMagicItems\L10n" || goto :error
-xcopy /E /I /Y ..\..\..\Icons "C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Kingmaker\Mods\CraftMagicItems\Icons" || goto :error
-xcopy /E /I /Y ..\..\..\Data "C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Kingmaker\Mods\CraftMagicItems\Data" || goto :error
+"C:\Program Files\7-Zip\7z.exe" x CraftMagicItems.zip -y -o"C:\Program Files (x86)\Steam\steamapps\common\Pathfinder Kingmaker\Mods" || goto :error
 
 goto :EOF
 
