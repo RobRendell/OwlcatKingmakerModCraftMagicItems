f8fb7b847bff72a5c3c739eadece2b06dadfdd6a
Rob Rendell <rob.rendell.au@gmail.com>
2019-03-11 09:53:39 +1100

    Update to UMM 0.14.1

--- CraftMagicItems/Main.cs
+++ CraftMagicItems/Main.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using System.Text;
 using Kingmaker;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
